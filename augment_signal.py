0b03b284cb15ac60700ce3e08cb104df1105f947
MariaBec4 <159092161+MariaBec4@users.noreply.github.com>
2024-02-11 18:36:35 -0500

    Update augment_signal.py

--- augment_signal.py
+++ augment_signal.py
@@ -4,8 +4,23 @@ import numpy as np
 path = "Sound Recordings/crazy_fredrick.wav"
 x, sr = librosa.load(path)
 
-left = x.tolist()
-right = [0, 0, 0] + x.tolist()
+attenuation = [0.25, 0.5, 1.0]
 
-print(left[0:10])
-print(right[0:10])
+#Sampling rate: 44100 Hz --> T = 0.0227 ms
+for i in range(0,3)
+  delay = 10^i
+  delayNum = (int)(delay/0.0227)
+  zeros = [0]*delayNum
+  left = x.tolist() + zeros
+  right = zeros + x.tolist()
+
+  name = "Sound Recordings/crazy_fredrick" + str(delay) + "ms-"
+
+  for a in attenuation
+    right = a*right
+    name = name + str(a*6) + "dB.wav"
+  
+
+
+#print(left[0:10])
+#print(right[0:10])
